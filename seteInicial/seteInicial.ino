d3c164a90942413bea39c07fbf00a985f0fd2122
agus921 <agustinnavarro8@gmail.com>
2019-12-30 18:10:52 -0300

    test

--- seteInicial/seteInicial.ino
+++ seteInicial/seteInicial.ino
@@ -1,5 +1,6 @@
 /*Para el relay primera pata positivo segunda va al arduino y tercera al negativo, la referencia es (- + s)*/
 void setup() {
+    pinMode(0, OUTPUT);
     pinMode(1, OUTPUT);
     pinMode(2, OUTPUT);
     pinMode(3, OUTPUT);
@@ -16,25 +17,34 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite (1, HIGH);/*luz BLANCA*/
-  digitalWrite (6, HIGH);/*Ventilador led BLANCA*/
+  digitalWrite (0, LOW);/*NADA*/
+  digitalWrite (13, LOW);/*NADA*/
+  digitalWrite (1, HIGH);/*luz AMARILLA*/
+  digitalWrite (6, HIGH);/*Ventilador led AMARILLA*/
   digitalWrite (2, HIGH);/*luz VIOLETA*/
   digitalWrite (8, HIGH);/*Ventilador led VIOLETA*/
-  digitalWrite (7, HIGH);/*luz AMARILLA*/
-  digitalWrite (4, HIGH);/*Ventilador led AMARILLA*/
+  digitalWrite (7, HIGH);/*luz BLANCA*/
+  digitalWrite (4, HIGH);/*Ventilador led BLANCA*/
   
   
   digitalWrite (5, LOW);/*vALVULA INTERIOR*/
 
   
-  digitalWrite (3, LOW);/*Extractor*/
+  digitalWrite (3, HIGH);/*Extractor*/
 
   
   digitalWrite (9,  LOW);/*vALVULA A*/
   digitalWrite (10, LOW);/*vALVULA B*/
   digitalWrite (11, LOW);/*vALVULA C*/
   digitalWrite (12, LOW);/*vALVULA D*/
-  digitalWrite (13, LOW);/*Ventilador led AMARILLA*/
-  
-  
+
+  int lecturaA0 = analogRead(A0);/* MEDIDOR MEDIO*/
+  int lecturaPorcentajeA0 = map(lecturaA0,1023,0,0,100);
+
+  int lecturaA2 = analogRead(A2);/* MEDIDOR ALTO*/
+  int lecturaPorcentajeA2 = map(lecturaA2,1023,0,0,100);
+
+  int lecturaA1 = analogRead(A1);/* MEDIDOR BAJO*/
+  int lecturaPorcentajeA1 = map(lecturaA1,1023,0,0,100);
 }
+  
